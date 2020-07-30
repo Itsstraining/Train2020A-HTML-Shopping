3a9f05ebe0ca667de138d658272764be319bd19d
An <hoangan10012@gmail.com>
2020-07-30 18:01:35 +0700

    update shopping

--- SpiralMatrix.js
+++ SpiralMatrix.js
@@ -11,14 +11,14 @@ function createTable(size, defaultValue) {
 }
 
 let vector = {
-  x: 0,
-  y: -1,
+  x: 1,
+  y: 0,
 };
 
 function rotate(vector) {
   let result = {
-    x: -vector.y,
-    y: vector.x,
+    x: vector.y,
+    y: -vector.x,
   };
   return result;
 }
@@ -59,8 +59,8 @@ function spiral(size, vector) {
   let matrix = createTable(size, 0);
   let step = 1;
   let current = {
-    x: Math.floor(size / 2) - 1,
-    y: Math.floor(size / 2),
+    x: Math.ceil(size / 2)-1 ,
+    y: Math.ceil(size / 2),
   };
   console.log(current);
   while (checkCollision(current, size)) {
