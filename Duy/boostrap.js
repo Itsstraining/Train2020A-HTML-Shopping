a161e3747d0a826c72fccd152fcafa1f3e6f4289
Anh Duy <anhduy.nguyen0211@gmail.com>
2020-07-31 16:57:56 +0700

    Calculator

---
+++ Duy/boostrap.js
@@ -0,0 +1,9 @@
+function SinFunction(x) {
+    let show = document.getElementById("sin").innerHTML = Math.sin(x);
+    return show;
+}
+
+function CosFunction(y) {
+    let show = document.getElementById("cos").innerHTML = Math.cos(y);
+    return show;
+}
