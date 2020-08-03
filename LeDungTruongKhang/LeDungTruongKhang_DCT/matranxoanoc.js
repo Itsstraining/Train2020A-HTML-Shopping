f52734684ab3d60f31d0dc6113b92bec0ec3924a
TruongKhangLe <kangkedd@gmail.com>
2020-08-03 17:02:27 +0700

    CloneGoogle

---
+++ LeDungTruongKhang/LeDungTruongKhang_DCT/matranxoanoc.js
@@ -0,0 +1,10 @@
+function createTable(size,DefV){
+    let tb = [];
+    for(let i = 0 ; i < size ; i++){
+        let arr = [];
+        for(let j = 0 ; j < size ; j++) {
+            arr.push(DefV);
+            
+        }
+    }
+}
